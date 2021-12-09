e498739c79eea5e6551e059c7e9b2e0062a42eaa
Thalia <thaliag@my.yorku.ca>
2021-12-09 01:11:29 -0500

    Added NPC colliders with input prompt

---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    private GameObject triggeringNPC;
+    private bool triggering;
+
+    public GameObject npcText;
+
+    void Start() {
+
+    }
+
+    void Update() {
+        if (triggering) {
+            // print("Player is triggering with " + triggeringNPC);
+
+            // Activate text
+            npcText.SetActive(true);
+
+            if(Input.GetKeyDown(KeyCode.E)) {
+                print("Congratulations!");
+            }
+        } else {
+            // Deactivate text
+            npcText.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "NPC") {
+            triggering = true;
+            triggeringNPC = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (other.tag == "NPC") {
+            triggering = false;
+            triggeringNPC = null;
+        }
+    }
+}
